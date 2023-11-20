57ee8703d2c88a1c6355734471005f3aadf762c0
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2023-11-20 15:04:59 +0100

    added new placeholders

--- php/shared/nav.php
+++ php/shared/nav.php
@@ -1,15 +1,17 @@
 <nav>
 
-    <div>
+    <div class="left_nav">
         <img src="./images/BESTLOGO.png" alt="Website Logo">
-        <a>text1</a>
-        <a>text2</a>
-        <a>text3</a>
+        <button>Homepage</button>
+        <button>Dashboard</button>
+        <button>Explore</button>
     </div>
 
-    <div class="search_bar_box">
+    <div class="right_nav">
         <label>search_bar_placeholder</label>
         <input type ="text" id="search_bar" placeholder="search bar" name="search_bar" maxlength="128">   
+
+        <button alt="Your profile">ph</button>
     </div>
 
 </nav>
