eb3a0ce4a35fda39eac4a2781b2ad9f8fabb4734
Simo64Terminator <simo64.tomasella@gmail.com>
2023-11-25 01:16:47 +0100

    Updated dbManager.php, probably completed dbQuery method

--- php/phpFunc/dbManager.php
+++ php/phpFunc/dbManager.php
@@ -10,29 +10,50 @@
         private $conn;
         
         function __construct() {
-            if ( !( $this->conn = mysqli_connect($this->dbServer, $this->username, $this->password, $this->dbName) )) {
+            if ( !( $this->conn = new mysqli($this->dbServer, $this->username, $this->password, $this->dbName) )) {
                 error_log ('Error: cannot connect to database', 3, '/SAW/SAWFinalProject/texts/errorLog.txt');
                 die ('Connection Error' . mysqli_connect_error());
             }  
         }
 
-        // TODO function
-        function dbQuery($stmtString, $params) {
+        function dbQuery($stmtString, $paramsTypes, $params) {
             
-            if ( !($stmt = $this->conn->prepare($stmtString)) ){
+            if ( !($stmt = $this->conn->prepare($stmtString)) ) {
                 error_log('Error: cannot prepare the following query -> ' . $stmtString);
                 die ('Server error' . $this->conn->error);
             }
-            
-            $stmt->bind_param(...$params);
 
-            $stmt->execute();
+            if ( !($stmt->bind_param($paramsTypes, ...$params)) ) {
+                error_log('Error: cannot bind the following parameters -> ' . $params);
+                die ('Server error' . $this->conn->error);
+            }
 
-            $result = $stmt->get_result();
+            if ( !($stmt->execute() ) ) {
+                error_log('Error: cannot execute the following query -> ' . $stmtString);
+                die ('Server error' . $this->conn->error);
+            }
+
+
+            if ( str_contains($stmtString, 'SELECT') )
+                $result = $stmt->get_result();
+            else
+                $result = $stmt->affected_rows;
 
             $stmt->close();
 
             return $result;
+        }
+
+        function closeConnection() {
+            $this->conn->close();
+        }
+
+        function registerUser() {
+
+        }
+
+        function loginUser() {
+
         }
         
 
