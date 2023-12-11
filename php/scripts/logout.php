0eddd1390b8d2c7b785b435f6688c12e524a88ff
thomasghionesanremo@gmail.com <thomasghionesanremo@gmail.com>
2023-12-11 11:30:07 +0100

    minor change

--- php/scripts/logout.php
+++ php/scripts/logout.php
@@ -2,8 +2,6 @@
     require_once("../phpClasses/sessionManager.php");
     $sessionManager = new sessionManager();
 
-    $sessionManager->startSession();
-
     $sessionManager->endSession();
 
     header('Location: /SAW/SAWFinalProject/index.php');
