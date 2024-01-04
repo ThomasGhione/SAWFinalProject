4953f29a46d304d84045c32780e055a9b25e95a9
Simo64Terminator <simo64.tomasella@gmail.com>
2024-01-04 16:48:26 +0100

    Small changes in update_password

--- php/scripts/update_password.php
+++ php/scripts/update_password.php
@@ -19,23 +19,20 @@
         }
 
         if (empty($_POST["oldPassword"]) || (empty($_POST["newPassword"]))) {
-            error_log("Invalid request", 3, "/SAW/SAWFinalProject/texts/errorLog.txt");
-            throw new Exception("Invalid request");
+            error_log("At least one of the fields in update_password is empty", 3, "/SAW/SAWFinalProject/texts/errorLog.txt");
+            throw new Exception("No fields can be empty");
         }
 
         if (!$dbManager->updatePassword($sessionManager->getEmail())) {
             error_log("Something went wrong while updating password", 3, "/SAW/SAWFinalProject/texts/errorLog.txt");
             throw new Exception("Something went wrong while updating password. Please try again later");
         }
-    }
-    catch (Exception $e) {
-        $_SESSION["error"] = $e->getMessage();
-        header("Location: ../update_profile_password_form.php");
-        exit;
-    }
 
-    
-    $_SESSION["success"] = "Your changes were applied successfully!";
+        $_SESSION["success"] = "Your changes were applied successfully!";
+
+    }
+    catch (Exception $e) { $_SESSION["error"] = $e->getMessage();}
+
     header("Location: ../update_profile_password_form.php"); // Covers both invalid request and invalid login 
     exit;
 
