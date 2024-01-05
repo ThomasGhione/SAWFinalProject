ba21dec4d3a4927626851d87f0f21969f8f54a17
Simo64Terminator <simo64.tomasella@gmail.com>
2024-01-05 12:34:50 +0100

    Added some options for editing user, fixed a bug (permissions to access editUserForm and checking on [email]), changed file position of editUserForm.php

---
+++ php/adminTools/editUserForm.php
@@ -0,0 +1,88 @@
+<?php 
+    require("../shared/initializePage.php");
+
+    if (!$sessionManager->isSessionSet() || !$sessionManager->isAdmin()) {
+        header("Location: ../../index.php");
+        exit;
+    }
+
+    if (!isset($_GET["email"]) || empty($_GET["email"]) || !filter_var(urldecode($_GET["email"]), FILTER_VALIDATE_EMAIL)) {
+        header("Location: ./manageUsers.php");
+        exit;
+    }
+
+    $email = urldecode($_GET["email"]);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("../shared/commonHead.php") ?>
+    <title>OpenHub - Update user page</title>
+</head>
+<body>
+    <?php include("../shared/nav.php"); ?>
+
+    
+    <main class="mainContainer">
+
+        <fieldset class="formSection">
+            <h2>Edit profile here:</h2>
+                        
+            <?php
+                if (isset($_SESSION["error"])) {
+                    echo "<p class='error'>" . $_SESSION["error"] . "</p>";
+                    unset($_SESSION["error"]);
+                }
+                elseif (isset($_SESSION["success"])) {
+                    echo "<p class='success'>" . $_SESSION["success"] . "</p>";
+                    unset($_SESSION["success"]);
+                }
+            ?>
+
+            <form action="./adminScripts/editUser.php" method="post">
+                            
+                <div class="inputBox">
+                    <label for="firstname">Firstname: </label>
+                    <input type="text" id="firstname" name="firstname" placeholder="Firstname">
+                </div>
+                
+                <div class="inputBox">
+                    <label for="lastname">Lastname: </label>
+                    <input type="text" id="lastname" name="lastname" placeholder="Lastname">
+                </div>
+
+                <div class="inputBox">
+                    <label for="newEmail">Email: </label>
+                    <input type="email" id="newEmail" name="newEmail" placeholder="Email">
+                </div>
+
+                <div class="inputBox">
+                    <label for="pass">Password: </label>
+                    <input type="password" id="pass" name="pass" placeholder="Password">
+                </div>
+
+                <div class="inputBox">
+                    <label for="permission">Role: </label>
+                    <select id="permission" name="permission">
+                        <option value="user">User</option>
+                        <option value="admin">Admin</option>
+                    </select>
+                </div>
+
+                <input type="hidden" name="email" value="<?php echo htmlspecialchars($_GET["email"])?>">
+
+                <input type="submit" class="formButton" name="submit" value="Edit">
+            </form>
+
+            <a href="./manageUsers.php" class="formButton">Go back to users list</a>
+
+        </fieldset>
+
+    </main>
+
+
+    <?php include("../shared/footer.php"); ?>
+</body>
+</html>
