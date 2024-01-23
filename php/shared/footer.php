5032d288afdc27ce48431b1647366900e57e3534
Simo64Terminator <simo64.tomasella@gmail.com>
2024-01-23 12:21:09 +0100

    Small change in footer

--- php/shared/footer.php
+++ php/shared/footer.php
@@ -8,12 +8,10 @@
         <p>&copy; 2023 - All rights reserved - <a class="footerLink" href="<?php echo $root?>/php/termsAndConditions.php">See our terms and conditions</a></p>
         <i>This site is a great replacement for an ancient way to manage repositories.</i>
         <a class="footerLink" href="<?php echo $root?>/php/shared/listOfReasons.php">Learn more about this</a>
-        
-        <a> - </a>
 
         <?php
             if (isset($_SESSION["newsletter"]) && !$_SESSION["newsletter"])
-                echo "<a class='footerLink' href='$root/php/scripts/manageUserInNewsletter.php?sub=" . "true" . "'>Join our newsletter!</a>";
+                echo "<a> - </a><a class='footerLink' href='$root/php/scripts/manageUserInNewsletter.php?sub=" . "true" . "'>Join our newsletter!</a>";
         ?>
     </address>
     
